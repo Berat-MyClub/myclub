0b93148ca7ae2bb07dbed836027ea3b61a98f2c7
Berat MyClub <b.uzun@myclub.dance>
2022-08-18 03:36:25 +0200

    fix: yarn build

--- pages/searchSong.js
+++ pages/searchSong.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import Wrapper from "../src/components/Global/Wrapper";
-import EventSearchBar from "../src/components/Home/EventSearchBar";
 import SearchInput from "../src/components/Voting/SearchInput";
 import spotifyApi from "../lib/spotify";
 import { useSession } from "next-auth/react";
